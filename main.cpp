2fef19340fb4787b23753e9bd135957762416fc2
crank_ocial <kirill201098@mail.ru>
2018-11-15 13:09:45 +0300

    zad4

--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ int main() {
     int a3 = a%100/10; //5
     int a4 = a%1000/100; //4
     std::cout << a2 << a3 << a4 << a1 << std::endl;
+    a1 +=5;
+    a2 +=5;
+    a3 +=5;
+    a4 +=5;
+    std::cout << a1 << a4 << a3 << a2 << std::endl;
+    a1 -= 1;
+    a2 -= 4;
+    a3 -= 3;
+    a4 -= 2;
+    std::cout << a1 << a4 << a3 << a2 << std::endl;
+    int b;
+    std::cout << "b=" << a1 << a4 << a3 << a2 << std::endl;
     return 0;
 }
