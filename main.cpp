76777bf667abb112ea4380bfe5ef770e27d7ef2d
crank_ocial <kirill201098@mail.ru>
2018-11-15 13:32:24 +0300

    zad1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
 
 int main() {
-    int a = 8456;
-    int a1 = a/1000; //8
-    int a2 = a%10; //6
-    int a3 = a%100/10; //5
-    int a4 = a%1000/100; //4
-    std::cout << a2 << a3 << a4 << a1 << std::endl;
-    a1 +=5;
-    a2 +=5;
-    a3 +=5;
-    a4 +=5;
-    std::cout << a1 << a4 << a3 << a2 << std::endl;
-    a1 -= 1;
-    a2 -= 4;
-    a3 -= 3;
-    a4 -= 2;
-    std::cout << a1 << a4 << a3 << a2 << std::endl;
-    int b;
-    std::cout << "b=" << a1 << a4 << a3 << a2 << std::endl;
+    float a = 40;
+    float b = 50;
+    std::cout << a/7 << std::endl;
+    std::cout << b/7 << std::endl;
     return 0;
 }
